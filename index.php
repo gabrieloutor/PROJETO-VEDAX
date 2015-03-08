307889989517bf2f0d9816c58bde62a4ba0effd6
GabrielOutor <GabrielOutor@10.0.0.106>
2015-03-08 16:05:38 -0300

    PAGINA INDEX ADICIONADA

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo '<!DOCTYPE html>
+<html>
+    <head>
+        <title>VEDAX</title> 
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        TESTE
+    </body>
+</html>';
+?>
