b1ae9f3ac3696e829d1717a4dc2b1ecc4759e4e6
41403037 <41403037@089970-P33S12.macklabs.br>
2015-03-09 11:30:17 -0300

    Tags Meta adicinadas

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
-<?php
-echo '<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <title>VEDAX</title> 
-        <meta charset="utf-8">
+        <meta charset="UTF-8">
+        <meta name="description" content="Vedax - A Vedax é uma empresa estabelecida no mercado desde 1975, especializada na fabricação de forjados, na linha de Flanges, Conexões tubulares e Forjados Especiais nos materiais Aço Liga, Inox e Carbono, obedecendo as especificações de normas nacionais e internacionais de fabricação."/>
+        <meta name="keywords" content="vedax,aço,conexões,flanges,forjados,carbono,empresa">
+        <meta name="author" content="Gabriel Outor">
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
     <body>
         TESTE
     </body>
-</html>';
+</html>
