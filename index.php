d7867612902541187026f7a6da89ae563c35efa5
41403037 <41403037@089970-P33S12.macklabs.br>
2015-03-09 12:08:54 -0300

    Arquivo CSS adicionado

--- index.php
+++ index.php
@@ -1,14 +1,66 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>VEDAX</title> 
-        <meta charset="UTF-8">
-        <meta name="description" content="Vedax - A Vedax é uma empresa estabelecida no mercado desde 1975, especializada na fabricação de forjados, na linha de Flanges, Conexões tubulares e Forjados Especiais nos materiais Aço Liga, Inox e Carbono, obedecendo as especificações de normas nacionais e internacionais de fabricação."/>
-        <meta name="keywords" content="vedax,aço,conexões,flanges,forjados,carbono,empresa">
-        <meta name="author" content="Gabriel Outor">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
-    <body>
-        TESTE
-    </body>
+	<head>
+		<title>
+			VEDAX
+		</title>
+		<meta charset="UTF-8">
+		<meta name="description" content="Vedax - A Vedax é uma empresa estabelecida no mercado desde 1975, especializada na fabricação de forjados, na linha de Flanges, Conexões tubulares e Forjados Especiais nos materiais Aço Liga, Inox e Carbono, obedecendo as especificações de normas nacionais e internacionais de fabricação."/>
+		<meta name="keywords" content="vedax,aço,conexões,flanges,forjados,carbono,empresa">
+		<meta name="author" content="Gabriel Outor">
+		<link rel="stylesheet" type="text/css" href="/projeto-vedax/css/style.css">
+	</head>
+	<body>
+            <nav>
+		<ul class="menu">
+			<li class="menuhome">
+				<a href="/projeto-vedax/">Home</a>
+			</li>
+			<li class="menuavedax">
+				<a href="/projeto-vedax/a-vedax.php">A Vedax</a>
+			</li>
+			<li class="menuprodutos">
+				<a href="/projeto-vedax/produtos.php">Produtos</a>
+				<ul>
+					<li class="menuflanges">
+						<a href="/projeto-vedax/flanges.php">Flanges</a>
+					</li>
+					<li class="menurotativos">
+						<a href="/projeto-vedax/flanges-rotativos.php">Flanges Rotativos</a>
+					</li>
+					<li class="menuaforjados">
+						<a href="/projeto-vedax/aneis-forjados.php">Anéis Forjados</a>
+					</li>
+					<li class="menudforjados">
+						<a href="/projeto-vedax/discos-forjados.php">Discos Forjados</a>
+					</li>
+					<li class="menufespeciais">
+						<a href="/projeto-vedax/forjados-especiais.php">Forjados Especiais</a>
+					</li>
+					<li class="menuconexoes">
+						<a href="/projeto-vedax/conexoes-forjados.php">Conexões Forjados</a>
+					</li>
+				</ul>
+			</li>
+			<li class="menuservico">
+				<a href="/projeto-vedax/servico.php">Serviço</a>
+				<ul>
+					<li class="menutratamentotermico">
+						<a href="/projeto-vedax/tratamento-termico.php">Tratamento Térmico</a>
+					</li>
+					<li class="menusoldagem">
+						<a href="/projeto-vedax/soldagem.php">Soldagem</a>
+					</li>
+				</ul>
+			</li>
+			<li class="menumercado">
+				<a href="/projeto-vedax/mercado.php">Mercado</a>
+			</li>
+			<li class="menufaleconosco">
+				<a href="/projeto-vedax/fale-conosco.php">Fale Conosco</a>
+			</li>
+		</ul>
+            </nav>
+	</body>
+
 </html>
