0d0737524ae70684e74b890439507bedea070377
GabrielOutor <GabrielOutor@10.0.0.106>
2015-04-01 22:31:10 -0300

    Email config

--- envia.php
+++ envia.php
@@ -1,21 +1,21 @@
-<?php
-// Send an email with the Postmark-PHP library 
-// Learn more -> http://developer.postmarkapp.com/developer-official-libs.html#php
-
-// Install with composer
-composer require wildbit/postmark-php
-
-//Import
-use Postmark\PostmarkClient;
-
-// Example request
-$client = new PostmarkClient("21720715-11a8-4797-a056-73a07021a4f2");
-
-$sendResult = $client->sendEmail(
-  "sender@example.org",
-  "gabriel.outor@hotmail.com",
-  "Hello from Postmark!",
-  "This is just a friendly 'hello' from your friends at Postmark."
-);
-echo "chegou";
+<?php 
+//1 – Definimos Para quem vai ser enviado o email 
+$para = "gabriel.outor@hotmail.com"; 
+//2 - resgatar o nome digitado no formulário e grava na variavel $nome 
+$nome = $_POST['nome']; 
+// 3 - resgatar o assunto digitado no formulário e grava na variavel //$assunto 
+$assunto = $_POST['assunto']; 
+//4 – Agora definimos a mensagem que vai ser enviado no e-mail 
+$mensagem = "<strong>Nome: </strong>".$nome; 
+$mensagem .= "<br> <strong>Mensagem: </strong>".$_POST['mensagem']; 
+//5 – agora inserimos as codificações corretas e tudo mais. 
+$headers = "Content-Type:text/html; charset=UTF-8\n"; 
+$headers .= "From: dominio.com.br<sistema@dominio.com.br>\n"; //Vai ser //mostrado que o email partiu deste email e seguido do nome 
+$headers .= "X-Sender: <sistema@dominio.com.br>\n"; //email do servidor //que enviou 
+$headers .= "X-Mailer: PHP v".phpversion()."\n"; 
+$headers .= "X-IP: ".$_SERVER['REMOTE_ADDR']."\n"; 
+$headers .= "Return-Path: <sistema@dominio.com.br>\n"; //caso a msg //seja respondida vai para este email. 
+$headers .= "MIME-Version: 1.0\n"; 
+mail($para, $assunto, $mensagem, $headers); //função que faz o envio do email. 
+echo "ENVIADO COM SUCESSO";
 ?>
