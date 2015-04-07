6c0f1bf8093a643fb8d14b220ff10a82df83755f
GabrielOutor <GabrielOutor@10.0.0.103>
2015-04-07 19:58:48 -0300

    COMECANDO PAGINA DE PRODUTOS, TESTANDO...

--- produtos.php
+++ produtos.php
@@ -287,9 +287,33 @@
                     <div class="container">
                         <div class="content-inner row">
                             <div class="item_header">
-                                <h1>
-                                    Produtos
-                                </h1>
+                                <div class="main-content">
+                                    <div class="ruds">
+                                        <div class="text-item">
+
+                                            <div class="titles">
+                                                <h1>ON SHORE</h1>
+                                                <h2>OFFICINE AMBROGIO MELESI </h2>
+                                            </div>
+                                            <div class="block clearfix">
+                                                Products in compliance with the most known standards, as ASME code, fit  for both liquid and gas fluids transport and transformation.&nbsp; &nbsp;<br><br>   
+                                            </div>
+                                            <div class="block clearfix">
+                                                <table class="columns-3" width="100%" border="0" cellpadding="0" cellspacing="0">     <tbody>         <tr>             <td><h3>&nbsp; &nbsp; &nbsp; NIPO FLANGES&nbsp;</h3>             <p><img src="/media/Products/ApplicazioneOnshore/NIPO.jpg" alt=""></p></td>             <td><h3>&nbsp; &nbsp; &nbsp; &nbsp; Y PIECES</h3>             <p><img src="/media/Products/ApplicazioneOnshore/Y%20PIECES.jpg" alt=""></p></td>             <td><h3>&nbsp; ANCHOR FLANGES</h3>             <p><img src="/media/Products/ApplicazioneOnshore/ANCHOR%20FLANGES.jpg" alt=""></p></td>             <td><h3>&nbsp; &nbsp; &nbsp; &nbsp;ADAPTORS</h3><h3><p style="font-size: 12px; font-weight: normal;"><img src="/media/Products/ApplicazioneOnshore/ADAPTORS.jpg" alt=""></p></h3></td>         </tr>     </tbody> </table>   
+                                            </div>
+                                            <div class="block clearfix">
+                                                <table class="columns-3" cl="" width="100%" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td><h3>&nbsp; &nbsp; &nbsp; &nbsp;STUB ENDS</h3><p><img border="0" src="/media/Products/ApplicazioneOnshore/STUB_END.jpg" alt=""></p><p>&nbsp;</p></td><td><h3>&nbsp; &nbsp; &nbsp;SPOOL PIECES</h3><p><img alt="" src="/media/Products/ApplicazioneOnshore/Spoolpiece.jpg" border="0"></p><p>&nbsp;</p></td><td><h3>COMPACT FLANGES</h3><h3><p style="font-size: 12px; font-weight: normal;"><img alt="" src="/media/Products/ApplicazioneOnshore/COMPACT.jpg"></p></h3></td><td><h3>&nbsp; &nbsp; &nbsp; PENETRATOR</h3><p><img alt="" src="/media/Products/ApplicazioneOnshore/PENETRATOR.jpg" border="0"></p><p>&nbsp;</p></td></tr></tbody></table>   
+                                            </div>
+                                            <div class="block clearfix">
+                                                <table class="columns-3" width="100%" border="0" cellpadding="0" cellspacing="0">     <tbody>         <tr>             <td><h3>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;NOZZLES</h3>                         <p><img alt="" src="/media/Products/ApplicazioneOnshore/NOZZLE.jpg"></p></td>             <td><h3>&nbsp;SPECTACLE BLINDS – &nbsp; &nbsp; SPACER&amp;SPADE</h3>                          <p><img alt="" src="/media/Products/ApplicazioneOnshore/SPECTACLE.jpg"></p></td>             <td><h3>&nbsp; &nbsp; &nbsp; &nbsp; DRIP RINGS</h3>             <p><img src="/media/Products/ApplicazioneOnshore/DRIP%20RINGS.jpg" alt=""></p></td>             <td><h3>LONG&nbsp;WELDING&nbsp;NECK</h3>             <p>&nbsp;<img src="/media/Products/ApplicazioneOnshore/LONG_WN .jpg" alt="" border="0"></p></td>         </tr>     </tbody> </table>   
+                                            </div>
+                                            <div class="block clearfix">
+                                                <table cl="" class="columns-3" width="100%" border="0" cellpadding="0" cellspacing="0">     <tbody>         <tr>             <td><ul>                 <li>BUCKLE ARRESTORS</li>                 <li>SOCKET WELDING</li>             </ul></td>             <td><ul>                 <li>ORIFICE FLANGES</li>                 <li>THREADED&nbsp;</li>             </ul></td>             <td><ul>                 <li>WELDING NECK</li>                 <li>LAP JOINT</li>             </ul></td>             <td><ul>                 <li>BLIND</li>                 <li>SLIP ON</li>             </ul></td>         </tr>     </tbody> </table>\   
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
