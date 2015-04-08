1ff5fd66005a41c4407ba997bd40ede096f0130b
GabrielOutor <GabrielOutor@192.168.49.81>
2015-04-08 10:24:35 -0300

    TESTANDO DB

--- testedb.php
+++ testedb.php
@@ -1,22 +1,23 @@
 <?php
-$login =  getenv("LOGINDB");
-$senha =  getenv("SENHADB");
-$nome =  getenv("NOMEDB");
-$url =  getenv("URLDB");
-if($url==null){
-    $url="127.0.0.1";
+
+$login = getenv("LOGINDB");
+$senha = getenv("SENHADB");
+$nome = getenv("NOMEDB");
+$url = getenv("URLDB");
+if ($url == null) {
+    $url = "127.0.0.1";
 }
-$conexao = mysqli_connect($url,$login,$senha,$nome);
-  // tratamento de erros
-  if (mysqli_connect_errno())
-  {
-      echo "Não foi possível conectar: " . mysqli_connect_error();
-  }else{
-      echo "CONECTADO";
-  }
-  $resultado = mysqli_query($conexao, "SELECT * from EMPRESA");
-  if($resultado){
-      while($row = mysqli_fetch_array($resultado) ){
-         echo $row["NOME"]."<br/>";
-} }
-  ?>
+$conexao = mysqli_connect($url, $login, $senha, $nome);
+// tratamento de erros
+if (mysqli_connect_errno()) {
+    echo "Não foi possível conectar: " . mysqli_connect_error();
+} else {
+    echo "CONECTADO";
+}
+$resultado = mysqli_query($conexao, "SELECT * from EMPRESA");
+if ($resultado) {
+    while ($row = mysqli_fetch_array($resultado)) {
+        echo $row["NOME"] . "<br/>";
+    }
+}
+?>
