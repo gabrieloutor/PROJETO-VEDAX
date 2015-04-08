01c16b7408b28bc2b669e34412cf9bbfc49c2324
GabrielOutor <GabrielOutor@192.168.49.81>
2015-04-08 11:35:40 -0300

    TESTANDO CARACTER ESPECIAL

--- testedb.php
+++ testedb.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta charset="UTF-8" >
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Localização | TESTANDO CARACTER</title>
     </head>
     <body>
