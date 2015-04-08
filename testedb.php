87c61d7a191526d9144af9c6eac6e2edbcd78cc3
GabrielOutor <GabrielOutor@192.168.49.81>
2015-04-08 11:31:33 -0300

    TESTANDO CARACTER ESPECIAL

--- testedb.php
+++ testedb.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8" >
+    </head>
+    <body>
 <?php
 
 $login = getenv("LOGINDB");
@@ -21,3 +28,5 @@ if ($resultado) {
     }
 }
 ?>
+    </body>
+</html>
