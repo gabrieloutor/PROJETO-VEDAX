5a0d7e8c817fb4b3645aca151febdcef28562f12
GabrielOutor <GabrielOutor@10.0.1.13>
2015-04-10 21:57:48 -0300

    AJUSTANDO PAGINA DE PRODUTOS -- sem produtos

--- produtos.php
+++ produtos.php
@@ -292,14 +292,37 @@
                                         <div class="text-item">
 
                                             <div class="titles">
-                                                <h1>ON SHORE</h1>
-                                                <h2>OFFICINE AMBROGIO MELESI </h2>
+                                                <h1>PRODUTOS</h1>
+                                                <h2>FÁBRICA VEDAX</h2>
                                             </div>
                                             <div class="block clearfix">
-                                                Products in compliance with the most known standards, as ASME code, fit  for both liquid and gas fluids transport and transformation.&nbsp; &nbsp;<br><br>   
+                                                Produtos em conformidade com os padrões mais conhecidos, como o código ASME, apto para tanto líquido e gás de transporte de fluidos e transformação.&nbsp; &nbsp;<br><br>   
                                             </div>
                                             <div class="block clearfix">
-                                                <table class="columns-3" width="100%" border="0" cellpadding="0" cellspacing="0">     <tbody>         <tr>             <td><h3>&nbsp; &nbsp; &nbsp; NIPO FLANGES&nbsp;</h3>             <p><img src="/media/Products/ApplicazioneOnshore/NIPO.jpg" alt=""></p></td>             <td><h3>&nbsp; &nbsp; &nbsp; &nbsp; Y PIECES</h3>             <p><img src="/media/Products/ApplicazioneOnshore/Y%20PIECES.jpg" alt=""></p></td>             <td><h3>&nbsp; ANCHOR FLANGES</h3>             <p><img src="/media/Products/ApplicazioneOnshore/ANCHOR%20FLANGES.jpg" alt=""></p></td>             <td><h3>&nbsp; &nbsp; &nbsp; &nbsp;ADAPTORS</h3><h3><p style="font-size: 12px; font-weight: normal;"><img src="/media/Products/ApplicazioneOnshore/ADAPTORS.jpg" alt=""></p></h3></td>         </tr>     </tbody> </table>   
+                                                <table class="columns-3" width="100%" border="0" cellpadding="0" cellspacing="0">     
+                                                    <tbody>         
+                                                        <tr>             
+                                                            <td>
+                                                                <h3>&nbsp; &nbsp; &nbsp; &nbsp;</h3>             
+                                                                <p><img src="/media/Products/ApplicazioneOnshore/NIPO.jpg" alt=""></p>
+                                                            </td>             
+                                                            <td>
+                                                                <h3>&nbsp; &nbsp; &nbsp; &nbsp; Y PIECES</h3>             
+                                                                <p><img src="/media/Products/ApplicazioneOnshore/Y%20PIECES.jpg" alt=""></p>
+                                                            </td>             
+                                                            <td>
+                                                                <h3>&nbsp; ANCHOR FLANGES</h3>             
+                                                                <p><img src="/media/Products/ApplicazioneOnshore/ANCHOR%20FLANGES.jpg" alt="">
+                                                                </p></td>             
+                                                            <td>
+                                                                <h3>&nbsp; &nbsp; &nbsp; &nbsp;ADAPTORS</h3>
+                                                                <h3><p style="font-size: 12px; font-weight: normal;">
+                                                                        <img src="/media/Products/ApplicazioneOnshore/ADAPTORS.jpg" alt=""></p>
+                                                                </h3>
+                                                            </td>         
+                                                        </tr>     
+                                                    </tbody> 
+                                                </table>   
                                             </div>
                                             <div class="block clearfix">
                                                 <table class="columns-3" cl="" width="100%" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td><h3>&nbsp; &nbsp; &nbsp; &nbsp;STUB ENDS</h3><p><img border="0" src="/media/Products/ApplicazioneOnshore/STUB_END.jpg" alt=""></p><p>&nbsp;</p></td><td><h3>&nbsp; &nbsp; &nbsp;SPOOL PIECES</h3><p><img alt="" src="/media/Products/ApplicazioneOnshore/Spoolpiece.jpg" border="0"></p><p>&nbsp;</p></td><td><h3>COMPACT FLANGES</h3><h3><p style="font-size: 12px; font-weight: normal;"><img alt="" src="/media/Products/ApplicazioneOnshore/COMPACT.jpg"></p></h3></td><td><h3>&nbsp; &nbsp; &nbsp; PENETRATOR</h3><p><img alt="" src="/media/Products/ApplicazioneOnshore/PENETRATOR.jpg" border="0"></p><p>&nbsp;</p></td></tr></tbody></table>   
