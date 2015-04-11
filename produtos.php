4ef55e42c0c2b50bda9858b40afd489402fbac84
GabrielOutor <GabrielOutor@10.0.1.13>
2015-04-11 12:38:57 -0300

    TESTANDO PRODUTOS

--- produtos.php
+++ produtos.php
@@ -322,15 +322,7 @@
                                                 </table>   
                                                                 <!--<h3><p style="font-size: 12px; font-weight: normal;">-->      
 
-                                            <div class="block clearfix">
-                                                <table class="columns-3" cl="" width="100%" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td><h3>&nbsp; &nbsp; &nbsp; &nbsp;STUB ENDS</h3><p><img border="0" src="/media/Products/ApplicazioneOnshore/STUB_END.jpg" alt=""></p><p>&nbsp;</p></td><td><h3>&nbsp; &nbsp; &nbsp;SPOOL PIECES</h3><p><img alt="" src="/media/Products/ApplicazioneOnshore/Spoolpiece.jpg" border="0"></p><p>&nbsp;</p></td><td><h3>COMPACT FLANGES</h3><h3><p style="font-size: 12px; font-weight: normal;"><img alt="" src="/media/Products/ApplicazioneOnshore/COMPACT.jpg"></p></h3></td><td><h3>&nbsp; &nbsp; &nbsp; PENETRATOR</h3><p><img alt="" src="/media/Products/ApplicazioneOnshore/PENETRATOR.jpg" border="0"></p><p>&nbsp;</p></td></tr></tbody></table>   
-                                            </div>
-                                            <div class="block clearfix">
-                                                <table class="columns-3" width="100%" border="0" cellpadding="0" cellspacing="0">     <tbody>         <tr>             <td><h3>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;NOZZLES</h3>                         <p><img alt="" src="/media/Products/ApplicazioneOnshore/NOZZLE.jpg"></p></td>             <td><h3>&nbsp;SPECTACLE BLINDS – &nbsp; &nbsp; SPACER&amp;SPADE</h3>                          <p><img alt="" src="/media/Products/ApplicazioneOnshore/SPECTACLE.jpg"></p></td>             <td><h3>&nbsp; &nbsp; &nbsp; &nbsp; DRIP RINGS</h3>             <p><img src="/media/Products/ApplicazioneOnshore/DRIP%20RINGS.jpg" alt=""></p></td>             <td><h3>LONG&nbsp;WELDING&nbsp;NECK</h3>             <p>&nbsp;<img src="/media/Products/ApplicazioneOnshore/LONG_WN .jpg" alt="" border="0"></p></td>         </tr>     </tbody> </table>   
-                                            </div>
-                                            <div class="block clearfix">
-                                                <table cl="" class="columns-3" width="100%" border="0" cellpadding="0" cellspacing="0">     <tbody>         <tr>             <td><ul>                 <li>BUCKLE ARRESTORS</li>                 <li>SOCKET WELDING</li>             </ul></td>             <td><ul>                 <li>ORIFICE FLANGES</li>                 <li>THREADED&nbsp;</li>             </ul></td>             <td><ul>                 <li>WELDING NECK</li>                 <li>LAP JOINT</li>             </ul></td>             <td><ul>                 <li>BLIND</li>                 <li>SLIP ON</li>             </ul></td>         </tr>     </tbody> </table>\   
-                                            </div>
+
 
                                         </div>
                                     </div>
