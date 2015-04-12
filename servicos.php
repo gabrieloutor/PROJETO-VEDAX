bc56006d47ac9efb832280d6da74e4b8ce4c34d5
GabrielOutor <GabrielOutor@10.0.1.13>
2015-04-12 14:32:40 -0300

    FOOTER EDITADO

--- servicos.php
+++ servicos.php
@@ -297,62 +297,7 @@
             </div>
         </div>
         <!-- INICIO FOOTER-->
-        <div id="footer">
-            <div id="footer-row">
-                <div class="container">
-                    <div class="row">
-                        <div class="footer-row-1">
-                            <div class="moduletable contactus ">
-                                <div class="moduletable-wrapper">
-                                    <div class="mod-custom mod-custom__contactus">
-                                        <h4>
-                                            <span class="facebook">
-                                                <a target="_blank" href="<?php echo $facebook; ?>">Facebook</a>
-                                            </span>
-                                        </h4>
-                                        <h4>
-                                            <span class="icon-phone">
-                                                &#9743; <?php echo $telefone; ?>
-                                                <br />
-
-                                            </span>
-                                        </h4>
-                                        <h4>
-                                            <span class="adress">
-                                                &#9758; <?php echo $endereco; ?>
-                                                <br />
-                                            </span>
-                                        </h4>
-                                        <h4>
-                                            <span class="icon-mail">
-                                                &#9993; Email: <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
-                                            </span>
-                                        </h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="copyright-row">
-                <div class="container">
-                    <div id="trademark">
-                        Copyright &copy; <?php echo date("Y"); ?> | <?php echo $empresa; ?>
-                    </div>
-                    <div id="copyright-menu" class="row">
-                        <div id="ascopy">
-                            <a href="<?php echo $linkfacedesenvolvedor; ?>" target="_blank">
-                                &nbsp;&nbsp;&nbsp;&nbsp;Desenvolvido por:&nbsp;<?php echo $nomedesenvolvedor; ?>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php include("footer.php"); ?>
         <!-- FIM FOOTER-->
-        <div id="back-top">
-            <a href="#"><span></span></a>
-        </div>
     </body>
 </html>
