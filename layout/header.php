b8cc30cf1bb1cbab5886bb053a87d3cd6ab3b5e5
GabrielOutor <GabrielOutor@172.20.10.6>
2015-04-13 07:59:53 -0300

    Ajustado diretorios para local

--- layout/header.php
+++ layout/header.php
@@ -6,41 +6,41 @@
         <meta name="author" content="<?php echo $nomedesenvolvedor; ?>">
         <title><?php echo $titulo; ?> | <?php echo $empresa; ?></title>
         <!-- INICIO FAVICON -->
-        <link rel="apple-touch-icon" sizes="57x57" href="../images/icons/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="../images/icons/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="../images/icons/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="../images/icons/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="../images/icons/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="../images/icons/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="../images/icons/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="../images/icons/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="../images/icons/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="../images/icons/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="../images/icons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="../images/icons/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="../images/icons/favicon-16x16.png">
-        <link rel="manifest" href="../images/icons/manifest.json">
-        <link rel="icon" href="../images/icons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="57x57" href="images/icons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="images/icons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="images/icons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="images/icons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="images/icons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="images/icons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="images/icons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="images/icons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="images/icons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="images/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="images/icons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="images/icons/favicon-16x16.png">
+        <link rel="manifest" href="images/icons/manifest.json">
+        <link rel="icon" href="images/icons/favicon.ico" />
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="../images/icons/ms-icon-144x144.png">
+        <meta name="msapplication-TileImage" content="images/icons/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
         <!-- FIM FAVICON -->
         <!-- INICIO CSS -->
         <?php if($pagina=="produtos.php"){
-            echo "<link rel=\"stylesheet\" href=\"../css/ruds.css\" type=\"text/css\" >";
+            echo "<link rel=\"stylesheet\" href=\"css/ruds.css\" type=\"text/css\" >";
         }
         ?>
-        <link rel="stylesheet" href="../css/bootstrap.css" type="text/css" >
-        <link rel="stylesheet" href="../css/tmpl.default.css" type="text/css" >
-        <link rel="stylesheet" href="../css/nivo-slider.min.css" type="text/css" >
-        <link rel="stylesheet" href="../css/default.css" type="text/css" >
-        <link rel="stylesheet" href="../css/media.1024.css" type="text/css" >
-        <link rel="stylesheet" href="../css/media.980.css" type="text/css" >
-        <link rel="stylesheet" href="../css/media.768.css" type="text/css" >
-        <link rel="stylesheet" href="../css/media.480.css" type="text/css" >
-        <link rel="stylesheet" href="../css/ext.default.css" type="text/css" >
-        <link rel="stylesheet" href="../css/ext.asmenu.css" type="text/css" >
-        <link rel="stylesheet" href="../css/ext.artslider.css" type="text/css" >
+        <link rel="stylesheet" href="css/bootstrap.css" type="text/css" >
+        <link rel="stylesheet" href="css/tmpl.default.css" type="text/css" >
+        <link rel="stylesheet" href="css/nivo-slider.min.css" type="text/css" >
+        <link rel="stylesheet" href="css/default.css" type="text/css" >
+        <link rel="stylesheet" href="css/media.1024.css" type="text/css" >
+        <link rel="stylesheet" href="css/media.980.css" type="text/css" >
+        <link rel="stylesheet" href="css/media.768.css" type="text/css" >
+        <link rel="stylesheet" href="css/media.480.css" type="text/css" >
+        <link rel="stylesheet" href="css/ext.default.css" type="text/css" >
+        <link rel="stylesheet" href="css/ext.asmenu.css" type="text/css" >
+        <link rel="stylesheet" href="css/ext.artslider.css" type="text/css" >
         <style type="text/css">
             .item_fulltext
             {
@@ -134,21 +134,21 @@
         <!-- FIM CSS -->
         <!-- INICIO JAVASCRIPT -->
         <?php if($pagina=="index.php"){
-            echo "<script src=\"../js/newrelic.js\" type=\"text/javascript\"></script>";
+            echo "<script src=\"js/newrelic.js\" type=\"text/javascript\"></script>";
         }
         ?>
-        <script src="../js/jquery.min.js" type="text/javascript"></script>
-        <script src="../js/jquery-noconflict.js" type="text/javascript"></script>
-        <script src="../js/jquery-migrate.min.js" type="text/javascript"></script>
-        <script src="../js/caption.js" type="text/javascript"></script>
-        <script src="../js/bootstrap.js" type="text/javascript"></script>
-        <script src="../js/jquery.isotope.min.js" type="text/javascript"></script>
-        <script src="../js/touch.gallery.js" type="text/javascript"></script>
-        <script src="../js/scripts.js" type="text/javascript"></script>
-        <script src="../js/jquery.nivo.slider.min.js" type="text/javascript"></script>
-        <script src="../js/script.js" type="text/javascript"></script>
-        <script src="../js/jquery.mobilemenu.js" type="text/javascript"></script>
-        <script src="../js/jquery.hovermenu.js" type="text/javascript"></script>
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/jquery-noconflict.js" type="text/javascript"></script>
+        <script src="js/jquery-migrate.min.js" type="text/javascript"></script>
+        <script src="js/caption.js" type="text/javascript"></script>
+        <script src="js/bootstrap.js" type="text/javascript"></script>
+        <script src="js/jquery.isotope.min.js" type="text/javascript"></script>
+        <script src="js/touch.gallery.js" type="text/javascript"></script>
+        <script src="js/scripts.js" type="text/javascript"></script>
+        <script src="js/jquery.nivo.slider.min.js" type="text/javascript"></script>
+        <script src="js/script.js" type="text/javascript"></script>
+        <script src="js/jquery.mobilemenu.js" type="text/javascript"></script>
+        <script src="js/jquery.hovermenu.js" type="text/javascript"></script>
         <script type="text/javascript">
             jQuery(window).on('load', function () {
                 new JCaption('img.caption');
