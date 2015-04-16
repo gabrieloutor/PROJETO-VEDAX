a0f43c3fe01af1d327cdc18eef842f506292601b
GabrielOutor <GabrielOutor@10.0.1.6>
2015-04-16 18:50:47 -0300

    Ajustado título em Aneis e Discos

--- aneis.php
+++ aneis.php
@@ -2,6 +2,6 @@
     $tipopeca=3;
     $descricaoproduto="Produtos em conformidade com os padrões mais conhecidos, como o código ASME, apto para tanto líquido e gás de transporte de fluidos e transformação.";
     $tituloproduto="ANÉIS E DISCOS";
-    $titulo="Anéis - Produtos";
+    $titulo="Anéis e Discos- Produtos";
     include("produtos.php");
 ?>
