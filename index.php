a6705c13c7c513b7e30ab4de83db6da780c55abe
GabrielOutor <GabrielOutor@192.168.52.108>
2015-05-08 10:03:44 -0300

    New Relic Assistent added

--- index.php
+++ index.php
@@ -32,7 +32,9 @@
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
-
+        
+        <!-- NEW RELIC JS -->
+        <script src="js/newrelic.js" type="text/javascript"></script>
     </head>
 
     <body id="page-top" class="index">
