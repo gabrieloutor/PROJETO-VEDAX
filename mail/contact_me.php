3cfc40d98b95d83cbf7768b1a7ff4ab8ebe7543f
GabrielOutor <GabrielOutor@192.168.35.202>
2015-05-12 08:54:03 -0300

    Ajustes contato

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,18 +11,20 @@ $message = $_POST['message'];
 $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = $smtp;                     // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = $destinatario;   // SMTP username
+$mail->SMTPAuth = true;                            // Enable SMTP authentication
+$mail->Username = $emailcontato;               // SMTP username
 $mail->Password = $senha;                         // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable encryption, only 'tls' is accepted
+$mail->SMTPSecure = 'tls';                        // Enable encryption, only 'tls' is accepted
 
 $mail->From = $destinatario;
 $mail->FromName = 'VEDAX';
 $mail->addAddress($destinatario);                 // Add a recipient
 
 $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-$mail->Subject = 'MENSAGEM SITE VEDAX';
-$mail->Body = "MENSAGEM ENVIADA HORA-> $hora_envio DIA-> $data_envio
+$mail->Subject = 'Site VEDAX';
+$mail->Body = "MENSAGEM ENVIADA 
+            HORA-> $hora_envio 
+            DIA-> $data_envio
             Nome: $name 
             Email: $email 
             Telefone: $phone    
