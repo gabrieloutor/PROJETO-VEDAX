8d8560b70cdf6f7c93889111ecea92e85ad7005a
GabrielOutor <GabrielOutor@192.168.38.63>
2015-05-22 09:37:54 -0300

    Ajustado Index Admin

--- admin/index.php
+++ admin/index.php
@@ -32,30 +32,16 @@ if (!isset($_POST['login'])){
         <meta name="author" content="<?php echo $nomedesenvolvedor; ?>" >
         <title>Administração | <?php echo $empresa; ?></title>
         <link rel="icon" href="../img/favicon.ico" />
-
-        <!-- jQuery -->
         <script src="../js/jquery.js"></script>
-
-        <!-- Bootstrap Core JavaScript -->
         <script src="../js/bootstrap.min.js"></script>
-
-        <!-- Plugin JavaScript -->
         <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
         <script src="../js/classie.js"></script>
         <script src="../js/cbpAnimatedHeader.js"></script>
-
-        <!-- Custom Theme JavaScript -->
         <script src="../js/agency.js"></script>
-        
-        <!-- Bootstrap Core CSS -->
         <link href="../css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
         <link href="../css/agency.css" rel="stylesheet">
         <link href="../css/custom.css" rel="stylesheet">
         <link href="css/custom.css" rel="stylesheet">
-
-        <!-- Custom Fonts -->
         <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
         <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
@@ -64,8 +50,6 @@ if (!isset($_POST['login'])){
     </head>
     
     <body id="page-top" class="index">
-        <!-- Container -->
-        <!-- Contact Section -->
         <section id="contact">
             <div class="container">
                 <div class="row">
