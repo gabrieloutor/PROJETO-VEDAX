52b005ac00cd0d2629a1a4ec28460fc1dfa614bc
GabrielOutor <GabrielOutor@10.0.1.8>
2015-06-01 23:07:07 -0300

    Ajustes de CSS

--- index.php
+++ index.php
@@ -457,11 +457,7 @@ echo "<li>
 </div>
 <div class='col-md-4'>
 <ul class='list-inline social-buttons'>
-<li><a href='#'><i class='fa fa-twitter'></i></a>
-</li>
 <li><a href='<?php echo $facebook ?>' target='_blank'><i class='fa fa-facebook'></i></a>
-</li>
-<li><a href='#'><i class='fa fa-linkedin'></i></a>
 </li>
 </ul>
 </div>
