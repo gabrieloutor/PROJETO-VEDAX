d2c3973cdb50d8fe49484c1cf2f3912a2fe4909e
GABRIEL <GABRIEL@192.168.1.64>
2015-07-16 11:34:39 -0300

    Adicionado Google Analytics

--- index.php
+++ index.php
@@ -71,6 +71,16 @@ $ano=date("Y");
             js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.3";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-65233045-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <style type="text/css">
 ::-webkit-scrollbar {width:10px;}
 ::-webkit-scrollbar-track {background:#efefef;-webkit-box-shadow:inset 0 0 4px rgba(0,0,0,0.1);}
