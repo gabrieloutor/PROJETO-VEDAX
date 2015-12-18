bd2668e1f55204ec06afb4c6327490b43fba257b
Gabriel Outor <gabriel.outor@hotmail.com>
2015-12-18 13:10:42 -0200

    Teste Email 2

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -38,6 +38,7 @@ $mail->Body = "
     <html>
     <head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <meta http-equiv='content-type' content='text/html; charset=iso-8859-1' />
         <style>
             body{
                 font-family: 'Calibri, Arial, Helvetica, sans-serif';
