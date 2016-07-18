3dedbb747a5bdd67ae7e6ef39bca6ffa053f670a
Gabriel Outor <gabriel.outor@hotmail.com>
2016-07-18 11:37:43 -0300

    Voltado ao normal ( Logo )

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ $descricao=replaceaccents($row["descricao$db"]);
 <span class='icon-bar'></span>
 </button>
 <a class='navbar-brand page-scroll' href='#page-top' >
-<img src='img/logos/logovedaxa.png' alt='<?php echo $empresa ?>' id='vedaxlogo' title='Logo Vedax' />
+<img src='img/logos/logovedax.png' alt='<?php echo $empresa ?>' id='vedaxlogo' title='Logo Vedax' />
 </a>
 <a class='iconLing' href='<?php echo $urlLing ?>' >
 <img src='img/icons/<?php echo $img ?>' alt='icon' id='iconLinguagem' />
